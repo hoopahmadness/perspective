d7ae62af6d30819a7c43366c40e9894cec64c56b
Steve Hooper <hoopahmadness@gmail.com>
2022-11-13 09:31:00 -0500

    add some todo ideas

--- main.go
+++ main.go
@@ -7,3 +7,8 @@ import (
 func main() {
 	fmt.Println(parseDayStrings("Tuesday, Thur-Mon"))
 }
+
+// run as web server
+// read/write events to json file
+// read/write blocked days to json file
+// when serving, calculate urgency of task or tasks and return
