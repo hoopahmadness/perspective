a1b87718ae419d42857b9bc4ff8b9db75e494404
Steve Hooper <hoopahmadness@gmail.com>
2023-01-12 22:20:44 -0500

    rawLines system; output in organized sections; framework for computer generated text in output; add inactive events; clean up empty lines

--- generalEvents.go
+++ generalEvents.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 	"time"
 )
 
@@ -11,15 +12,31 @@ import (
 // Events can wrap around days or weeks (such as for Sleeping)
 type GeneralEvent struct {
 	// Friendly name for the event, i.e. Sleeping, School, Work, Church, etc
-	Name string `csv:"Name"`
+	Name string
 	// first, second, or both
-	Rotation rotation `csv:"Rotation"`
+	Rotation rotation
 	// a comma separated list of days of the week, also accepts hyphenated ranges and abbreviations
-	Days string `csv:"Days"`
+	Days string
 	// StartTime is in military time e.g. "18" is the one hour block starting at 6 pm
-	StartTime int `csv:"Hour"`
+	StartTime int
 	// Duration is the number of hours that an event lasts; round up when needed.
-	Duration int `csv:"Duration"`
+	Duration int
+	// Inactive is used to turn on and off events as needed, for example when traveling long term, without
+	// having to remove the events. Inactive events are not counted towards busy hours.
+	Inactive bool
+	Raw      string
+}
+
+func (e *GeneralEvent) AddRaw(line string) {
+	e.Raw += line + "\n"
+}
+
+func (e *GeneralEvent) PrintRaw() string {
+	return e.Raw
+}
+
+func (e *GeneralEvent) String() string {
+	return e.PrintRaw()
 }
 
 // Method for a GeneralEvent that returns a list of hourBlocks for an arbitrary 2 week rotation
@@ -45,4 +62,32 @@ func getNextBlockedHours(now time.Time, genEvents []*GeneralEvent) []int {
 	}
 	sort.Ints([]int(hourblocks))
 	return hourblocks
+}
+
+func outputEvents(eventList []*GeneralEvent) string {
+	outStr := ""
+	active := []*GeneralEvent{}
+	inactive := []*GeneralEvent{}
+	for _, event := range eventList {
+		if event.Inactive {
+			inactive = append(inactive, event)
+		} else {
+			active = append(active, event)
+
+		}
+	}
+	if len(active) != 0 {
+		outStr += fmt.Sprintf(headerLineFmt, repeatingEvents)
+		for _, task := range active {
+			outStr += task.PrintRaw()
+		}
+	}
+	if len(inactive) != 0 {
+		outStr += fmt.Sprintf(headerLineFmt, inactiveEvents)
+		for _, task := range inactive {
+			outStr += task.PrintRaw()
+		}
+	}
+	outStr = strings.Replace(outStr, "\n\n", "\n", -1)
+	return outStr
 }
